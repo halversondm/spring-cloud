9068d9b199e23f5b14fb3562610b1ab5b3686b09
Dan Halverson <daniel.m.halverson@gmail.com>
2018-02-25 13:18:42 -0600

    updated configuration for a successful start of the container components

--- docker/env/setenv.sh
+++ docker/env/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export CLASSPATH="${catalina.home}/env"
-export JAVA_OPTS="-Dspring.profiles.active=registry,customer,account"
+export CLASSPATH="${CATALINA_HOME}/env"
+export JAVA_OPTS="-Dspring.profiles.active=server"
